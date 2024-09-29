cb1057e14100bd4b444982c52e01aa50a46f5897
Hitesh Chotaliya <hiteshmistri08@gmail.com>
2024-09-29 10:12:20 +0530

    - Create Project

---
+++ Practice-SwiftUI/Practice-SwiftUI/Practice_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Practice_SwiftUIApp.swift
+//  Practice-SwiftUI
+//
+//  Created by Hitesh on 29/09/24.
+//
+
+import SwiftUI
+
+@main
+struct Practice_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
