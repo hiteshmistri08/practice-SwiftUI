4a2c030dd6ac711717729ce53b6984137c2db1a3
Hitesh Chotaliya <hiteshmistri08@gmail.com>
2024-11-01 11:32:23 +0530

    Present Forget Password Screen

--- Practice-SwiftUI/Practice-SwiftUI/Views/LoginView.swift
+++ Practice-SwiftUI/Practice-SwiftUI/Views/LoginView.swift
@@ -16,69 +16,74 @@ struct LoginView: View {
     @State private var canShowPasswordField: Bool = false
     @State private var isDisablePasswordButton: Bool = true
     
-    @State private var navigateToForgetPassword: Bool = false
+    @State private var presentForgetPassword: Bool = false
 
     var body: some View {
-        NavigationView {
-            VStack {
-                Image(systemName: "applelogo")
-                    .resizable()
-                    .frame(width: 50, height: 58)
-                    .scaledToFill()
-                Text("Welcome to Apple")
-                    .font(.callout)
-                    .foregroundColor(.gray)
-                VStack(alignment: .leading, spacing: 16.0) {
+        VStack {
+            Image(systemName: "applelogo")
+                .resizable()
+                .frame(width: 50, height: 58)
+                .scaledToFill()
+            Text("Welcome to Apple")
+                .font(.callout)
+                .foregroundColor(.gray)
+            VStack(alignment: .leading, spacing: 16.0) {
+                HStack(spacing: 5) {
+                    TextField("Enter your email id here...", text: $userName)
+                        .textFieldStyle(.plain)
+                        .keyboardType(.emailAddress)
+                        .onChange(of: userName) { newValue in
+                            isDisableEmailButton = !newValue.isValidEmail
+                            if newValue.isValidEmail == false {
+                                canShowPasswordField = false
+                                isHiddenEmailButton = false
+                            }
+                        }
+                    if isHiddenEmailButton == false {
+                        ArrowRightCircleBurron(isDisable: isDisableEmailButton) {
+                            canShowPasswordField = true
+                            isHiddenEmailButton = true
+                        }
+                    }
+                }
+                .padding()
+                .roundedRectengleOverlay()
+                
+                if canShowPasswordField {
                     HStack(spacing: 5) {
-                        TextField("Enter your email id here...", text: $userName)
+                        SecureField("Enter password here...", text: $password)
                             .textFieldStyle(.plain)
-                            .keyboardType(.emailAddress)
-                            .onChange(of: userName) { newValue in
-                                isDisableEmailButton = !newValue.isValidEmail
-                                if newValue.isValidEmail == false {
-                                    canShowPasswordField = false
-                                    isHiddenEmailButton = false
-                                }
+                            .onChange(of: password) { newValue in
+                                isDisablePasswordButton = !newValue.isValidPasswrod
                             }
-                        if isHiddenEmailButton == false {
-                            ArrowRightCircleBurron(isDisable: isDisableEmailButton) {
-                                canShowPasswordField = true
-                                isHiddenEmailButton = true
-                            }
+                        ArrowRightCircleBurron(isDisable: isDisablePasswordButton) {
+                            debugPrint("Login Successfully")
                         }
                     }
                     .padding()
                     .roundedRectengleOverlay()
                     
-                    if canShowPasswordField {
-                        HStack(spacing: 5) {
-                            SecureField("Enter password here...", text: $password)
-                                .textFieldStyle(.plain)
-                                .onChange(of: password) { newValue in
-                                    isDisablePasswordButton = !newValue.isValidPasswrod
-                                }
-                            ArrowRightCircleBurron(isDisable: isDisablePasswordButton) {
-                                debugPrint("Login Successfully")
-                            }
+                    HStack {
+                        Spacer()
+                        Button {
+                            debugPrint("Forgot button clicked")
+                            presentForgetPassword = true
+                        } label: {
+                            Text("Forget Password?")
                         }
-                        .padding()
-                        .roundedRectengleOverlay()
-                        
-                        HStack {
-                            Spacer()
-                            NavigationLink(destination: ForgetPasswordView(), isActive: $navigateToForgetPassword) {
-                                Button {
-                                    debugPrint("Forgot button clicked")
-                                    navigateToForgetPassword = true
-                                } label: {
-                                    Text("Forget Password?")
-                                }
+                        .sheet(isPresented: $presentForgetPassword) {
+                            NavigationView {
+                                ForgetPasswordView()
+                                    .navigationTitle("Forget Password")
+                                    
                             }
+                            .presentationDetents([.medium])
+                                
                         }
                     }
                 }
-                .padding()
             }
+            .padding()
         }
     }
 }
